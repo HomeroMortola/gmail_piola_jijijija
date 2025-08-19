f0e7e838eea3e6b722b2ec62a4ed53bfea779fc7
HomeroMortola <124947473+HomeroMortola@users.noreply.github.com>
2025-08-19 16:49:39 +0000

    prueva

---
+++ src/test/java/com/mycompany/app/MateriaTest.java
@@ -0,0 +1,16 @@
+public package com.mycompany.app;
+
+class MateriaTest {
+    Materia m1 = new Materia();
+    Alumno a1 = new Alumno();
+    m1.AgregarAlumno(a1);
+    m1.getAlumno().size
+
+    assertEquals("error no se encontro alumno",
+                1,
+                );
+
+
+
+    
+}
