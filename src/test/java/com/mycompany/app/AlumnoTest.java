248794fb343e4e09a18722ba00dfd59df0a6a813
HomeroMortola <124947473+HomeroMortola@users.noreply.github.com>
2025-08-20 13:49:09 +0000

    añadido fecha de nacimiento y ead en alumno

--- src/test/java/com/mycompany/app/AlumnoTest.java
+++ src/test/java/com/mycompany/app/AlumnoTest.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import java.time.LocalDate;
 public class AlumnoTest {
     public AlumnoTest() {
     }
@@ -19,6 +20,22 @@ public class AlumnoTest {
         assertEquals("Error nomnbre incorrecto",
                     "Paradigmas 2",
                      nombre);
+    }
+    @Test
+    public void nacionEn2006(){
+        Alumno a1 = new Alumno();
+        LocalDate fecha;
+        a1.setFechaNacimiento(LocalDate.of(2006,2,2));
+        fecha = a1.getFechaNacimiento();
+        assertEquals("Error Fecha no coinside",LocalDate.of(2006,2,2),fecha); 
+    }
+    @Test
+    public void edadEs19(){
+        Alumno a1 = new Alumno();
+        int edad;
+        a1.setFechaNacimiento(LocalDate.of(2006,2,2));
+        edad = a1.getEdad();
+        assertEquals("Error, edad no coinside",19,edad);
     }
 
 }
