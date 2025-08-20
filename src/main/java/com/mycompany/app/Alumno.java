0f1db75be714edd59d52b863abca831592b63651
HomeroMortola <124947473+HomeroMortola@users.noreply.github.com>
2025-08-20 03:13:44 +0000

    añadidodo de almno y alumno test

---
+++ src/main/java/com/mycompany/app/Alumno.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+public class Alumno {
+   private String nombre
+   public void setNombre(String _nombre){
+   
+   this.nombre = _nombre;
+
+   }
+
+
+   public String getNombre(){
+        return nombre;
+   }
+}
