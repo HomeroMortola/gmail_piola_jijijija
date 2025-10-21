b9c6c0ec99a822a245eccd4117215d73b4c49f86
HomeroMortola <homemortola@gmail.com>
2025-10-20 23:37:04 -0300

    Test Mail se recibe

--- src/main/java/com/mycompany/app/Mail.java
+++ src/main/java/com/mycompany/app/Mail.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 
 public class Mail {
     private ArrayList<Correo> bandejaEnviados;
+    private ArrayList<Correo> bandejaDeEntrada;
 
     public Mail() {
         bandejaEnviados = new ArrayList<Correo>();
+        bandejaDeEntrada = new ArrayList<Correo>();
     }
 
     public void setbandejaEnviados(ArrayList<Correo> bandejaEnviados) {
@@ -16,10 +18,21 @@ public class Mail {
         return bandejaEnviados;
     }
 
+    public void setbandejaDeEntrada(ArrayList<Correo> bandejaDeEntrada) {
+        this.bandejaDeEntrada = bandejaDeEntrada;
+    }
 
+    public ArrayList<Correo> getBandejaDeEntrada() {
+        return bandejaDeEntrada;
+    }
 
-    public void enviarCorreo(Correo correo) {
+    public void enviarCorreo(Correo correo, Mail destinatario) {
 
         bandejaEnviados.add(correo);
+        destinatario.recibirCorreo(correo);
+    }
+
+    public void recibirCorreo(Correo correo) {
+        bandejaDeEntrada.add(correo);
     }
 }
--- src/test/java/com/mycompany/app/MailTest.java
+++ src/test/java/com/mycompany/app/MailTest.java
@@ -9,10 +9,22 @@ public class MailTest {
     @Test
     public void testEnviarCorreo(){
         Mail m1 = new Mail();
+        
         Correo c1 = new Correo("Asunto","Contenido","Remitente","Destinatarios");
 
-        m1.enviarCorreo(c1);
+        m1.enviarCorreo(c1, m1);
 
         assertEquals(m1.getBandejaEnviados().get(0), c1);
+    }
+    @Test
+    public void testEnviarYRrecibirCorreo(){
+        Mail m1 = new Mail();
+        Mail m2 = new Mail();
+
+        Correo c1 = new Correo("Asunto","Contenido","Remitente","Destinatarios");
+
+        m1.enviarCorreo(c1,m2);
+
+        assertEquals(m2.getBandejaDeEntrada().get(0), c1);
     }
 }
