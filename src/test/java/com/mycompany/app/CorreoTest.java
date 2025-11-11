33f628be78c97fd994d8e753088953221fc048c5
Mordka Lucas Ayén <lucasmordka9@gmail.com>
2025-11-11 19:10:21 -0300

    Test datos de usuario

--- src/test/java/com/mycompany/app/CorreoTest.java
+++ src/test/java/com/mycompany/app/CorreoTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -11,4 +12,14 @@ public class CorreoTest {
         assertNotNull(c1);
     }
     
+    @Test
+    public void testDatosDeUsuario(){
+        Mail m1 = new Mail();
+        Contacto c1 = new Contacto("Nombre","nombre@outlook.com",m1);
+        m1.logIn(c1);
+
+        assertEquals("Nombre", c1.getNombre());
+        assertEquals("nombre@outlook.com", c1.getCorreo());
+    }
+
 }
